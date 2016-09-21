8d1c53850179af6528b152213d1d061dff7d4ae4
Andrew Fulton <andrewfulton9@gmail.com>
2016-09-21 11:14:28 -0600

    adding files

---
+++ app.py
@@ -0,0 +1,106 @@
+import os
+from flask import Flask, flash, render_template, request, redirect, \
+                  url_for, session, send_file
+from flask.ext.session import Session
+from werkzeug.utils import secure_filename
+import cPickle as pickle
+import pandas as pd
+from score_code import sa
+import tempfile
+import os
+import json
+
+ALLOWED_EXTENSIONS = set(['xlsx', 'xlsm', 'xlt'])
+
+app = Flask(__name__)
+sess = Session()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def build_scored_df(filename, rescore=None):
+    df = pd.read_excel(filename, index_col = 0, header = 0)
+    if df['pre_weight'].empty == False and \
+       df['post_weight'].empty == False:
+        weight_percentage = df['post_weight'] / df['pre_weight']
+        weight_percentage.name = 'weight_percentage'
+
+    if rescore == 'full':
+        scored = sa(df)
+    elif rescore == 'score_6':
+        scored = sa(df, rescore6 = True)
+    elif rescore == 'score_12':
+        scored = sa(df, rescore12 = True)
+
+    scored = pd.concat([df['group'], weight_percentage, scored], axis=1)
+    scored = scored.dropna(how = 'any', axis = 0)
+    return scored
+
+@app.route('/', methods = ['GET'])
+@app.route('/home', methods = ['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('file upload problem, please retry')
+            return redirect(request.url)
+        f = request.files['file']
+        if f.filename == '':
+            flash('no selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename):
+            rescore = request.form['rescore']
+            filename = secure_filename(f.filename)
+            print 'saving file'
+            UPLOAD_FOLDER = tempfile.mkdtemp()
+            session['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+            session['filename'] = filename
+            session['rescore'] = rescore
+            f.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('results'))
+    return render_template('upload.html')
+
+@app.route('/results', methods=['GET'])
+def results():
+    UPLOAD_FOLDER = session.get('UPLOAD_FOLDER')
+    name = session.get('filename')
+    rescore = session.get('rescore')
+    path = UPLOAD_FOLDER + '/' + name
+    scored_df = build_scored_df(path, rescore = rescore)
+    save_name = 'scored_{}.csv'.format(name[:-5])
+    saved_results = scored_df.to_csv(UPLOAD_FOLDER + '/' + save_name)
+    session['scored_path'] = UPLOAD_FOLDER + '/' + save_name
+    session['scored_filename'] = save_name
+    os.remove(path)
+    return render_template('results.html', name=name,
+                           f = scored_df.to_html())
+
+@app.route('/how_to', methods = ['GET', 'POST'])
+def how_to():
+    return render_template('how_to.html')
+
+@app.route('/download_template', methods = ['GET', 'POST'])
+def download_template():
+    return send_file('excel_template.xlsx',
+                     attachment_filename = 'input_template')
+
+@app.route('/download_results', methods = ['GET', 'POST'])
+def download_results():
+    scored_path = session.get('scored_path')
+    scored_name = session.get('scored_filename')
+    return send_file(scored_path, attachment_filename=scored_name)
+
+
+
+
+if __name__ == '__main__':
+    app.secret_key = 'secretest of keys'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
+    app.run(host='0.0.0.0', port = 8080, debug=True)
---
+++ score_code.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import collections
+
+
+#function to score spontaneous alternation
+def sa(df, start = 1, rescore = 76, rescore6 = False, rescore12 = False, rescore18=False):
+    df.columns = (str(column) for column in df.columns)
+    start = str(start)
+    rescore = str(rescore)
+
+    #build dataframe to hold rescored values
+    infoframe = pd.DataFrame(index = df.index,
+                             columns = ['alternations',
+                                        'arm entries',
+                                        '% alternation',
+                                        '% perseverative errors',
+                                        '% repeat entries',
+                                        '% 1 entries',
+                                        '% 2 entries',
+                                        '% 3 entries',
+                                        '% 4 entries',
+                                        'arm entry inequality'])
+
+    #score SA raw data from excel file and place in infoframe
+    for index in df.index:
+        if rescore6:
+            rescore = df['6min_arm'].ix[index]
+            try:
+                rescore = str(int(rescore))
+            except:
+                rescore = None
+        if rescore12:
+            rescore = df['12min_arm'].ix[index]
+            try:
+                rescore = str(int(rescore))
+            except:
+                rescore = None
+
+        if rescore:
+            drop = df.loc[:, start:rescore].ix[index].dropna(how='all')
+        if len(drop) > 4:
+            alts = 0
+            possible_alts = len(drop) - 3
+            pers = 0
+            repeat = 0
+            arm_dict = {1:0, 2:0, 3:0, 4:0}
+            for n in range(0, len(drop)):
+                if drop.iloc[n] not in [1, 2, 3, 4]:
+                    raise NameError('invalid arm entry input for subject %s on choice #%s: %s'
+                                     % (index, n, drop.iloc[n]))
+                arm_dict[drop.iloc[n]] += 1
+                if n >= 2:
+                    if drop.iloc[n] == drop.iloc[n-1]:
+                        repeat += 1
+                    if n >= 3:
+                        if drop.iloc[n] == drop.iloc[n-2]:
+                            pers +=1
+                        if n >= 4:
+                            alt_list = [drop.iloc[n], drop.iloc[n-1], drop.iloc[n-2], drop.iloc[n-3]]
+                            alt_count = collections.Counter(alt_list).values()
+                            if True not in map(lambda x: x > 1, alt_count):
+                                alts += 1
+            per_entry_diff = sum([abs(float(x)/len(drop) - .25)
+                                  for x in arm_dict.values()])
+            infoframe['alternations'][index] = int(alts)
+            infoframe['arm entries'][index] = len(drop)
+            infoframe['% alternation'][index] = float(alts)/possible_alts * 100
+            infoframe['% perseverative errors'][index] = float(pers)/(len(drop) - 2) * 100
+            infoframe['% repeat entries'][index] = float(repeat)/(len(drop) - 1) * 100
+            infoframe['% 1 entries'][index] = float(arm_dict[1]) / len(drop) * 100
+            infoframe['% 2 entries'][index] = float(arm_dict[2]) / len(drop) * 100
+            infoframe['% 3 entries'][index] = float(arm_dict[3]) / len(drop) * 100
+            infoframe['% 4 entries'][index] = float(arm_dict[4]) / len(drop) * 100
+            infoframe['arm entry inequality'][index] = per_entry_diff / 4 * 100
+
+    return infoframe.astype(float)
