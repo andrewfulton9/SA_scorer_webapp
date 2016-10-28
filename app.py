20ff994a4bd3aa0f1a778451d0ed469c6344d97e
Andrew Fulton <andrewfulton9@gmail.com>
2016-10-28 16:20:07 -0600

    added backend to download descriptive stats

--- app.py
+++ app.py
@@ -153,12 +153,16 @@ def results():
         try:
             scored_df, has_groups = build_scored_df(path, rescore = rescore)
             save_name = 'scored_{}.csv'.format(name[:-5])
-            saved_results = scored_df.to_csv(UPLOAD_FOLDER + '/' + save_name)
+            scored_df.to_csv(UPLOAD_FOLDER + '/' + save_name)
             session['scored_path'] = UPLOAD_FOLDER + '/' + save_name
             session['scored_filename'] = save_name
             os.remove(path)
             if has_groups:
                 described = get_descriptive_stats(scored_df)
+                ds_name = '{}_described.csv'.format(name[:-5])
+                described.to_csv(UPLOAD_FOLDER + '/' + ds_name)
+                session['described_path'] = UPLOAD_FOLDER + '/' + ds_name
+                session['described_filename'] = ds_name
                 return render_template('results.html', name=name,
                                        f = scored_df.to_html(),
                                        f1 = described.to_html(),
@@ -196,6 +200,14 @@ def download_results():
     return send_file(scored_path,
                      as_attachment = True,
                      attachment_filename=scored_name)
+
+@app.route('/download_descr', methods = ['GET', 'POST'])
+def download_descr():
+    descr_path = session.get('described_path')
+    descr_name = session.get('described_filename')
+    return send_file(descr_path,
+                     as_attachment = True,
+                     attachment_filename = descr_name)
 
 
 
