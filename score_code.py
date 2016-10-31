34688b927c27e234a9cc1ddebea60b409f6de5cf
Andrew Fulton <andrewfulton9@gmail.com>
2016-10-31 12:55:10 -0600

    moved scoring code from app to score code, made into class

--- score_code.py
+++ score_code.py
@@ -1,76 +1,165 @@
 import pandas as pd
+import numpy as np
 import collections
 
 
-#function to score spontaneous alternation
-def sa(df, start = 1, rescore = 76, rescore6 = False, rescore12 = False, rescore18=False):
-    df.columns = (str(column) for column in df.columns)
-    start = str(start)
-    rescore = str(rescore)
+class ScoreSA(object):
 
-    #build dataframe to hold rescored values
-    infoframe = pd.DataFrame(index = df.index,
-                             columns = ['alternations',
-                                        'arm entries',
-                                        '% alternation',
-                                        '% perseverative errors',
-                                        '% repeat entries',
-                                        '% 1 entries',
-                                        '% 2 entries',
-                                        '% 3 entries',
-                                        '% 4 entries',
-                                        'arm entry inequality'])
+    def __init__(filename, rescore):
+        self.filename = filename
+        self.rescore = rescore
+        self.raw_df = self.build_raw_df()
+        self._has_groups = False
+        self.scored_df = self.build_scored_df()
+        self.described_df = self.get_descriptive_stats()
 
-    #score SA raw data from excel file and place in infoframe
-    for index in df.index:
-        if rescore6:
-            rescore = df['6min_arm'].ix[index]
-            try:
-                rescore = str(int(rescore))
-            except:
-                rescore = None
-        if rescore12:
-            rescore = df['12min_arm'].ix[index]
-            try:
-                rescore = str(int(rescore))
-            except:
-                rescore = None
+    def build_raw_df():
+        df = pd.read_excel(self.filename, index_col=0, header = 0)
+        df.columns = (str(column) for column in df.columns)
+        df = self.convert_index(df)
+        return df
 
-        if rescore:
-            drop = df.loc[:, start:rescore].ix[index].dropna(how='all')
-        if len(drop) > 4:
-            alts = 0
-            possible_alts = len(drop) - 3
-            pers = 0
-            repeat = 0
-            arm_dict = {1:0, 2:0, 3:0, 4:0}
-            for n in range(0, len(drop)):
-                if drop.iloc[n] not in [1, 2, 3, 4]:
-                    raise NameError('invalid arm entry input for subject %s on choice #%s: %s'
-                                     % (index, n, drop.iloc[n]))
-                arm_dict[drop.iloc[n]] += 1
-                if n >= 2:
-                    if drop.iloc[n] == drop.iloc[n-1]:
-                        repeat += 1
-                    if n >= 3:
-                        if drop.iloc[n] == drop.iloc[n-2]:
-                            pers +=1
-                        if n >= 4:
-                            alt_list = [drop.iloc[n], drop.iloc[n-1], drop.iloc[n-2], drop.iloc[n-3]]
-                            alt_count = collections.Counter(alt_list).values()
-                            if True not in map(lambda x: x > 1, alt_count):
-                                alts += 1
-            per_entry_diff = sum([abs(float(x)/len(drop) - .25)
-                                  for x in arm_dict.values()])
-            infoframe['alternations'][index] = int(alts)
-            infoframe['arm entries'][index] = len(drop)
-            infoframe['% alternation'][index] = float(alts)/possible_alts * 100
-            infoframe['% perseverative errors'][index] = float(pers)/(len(drop) - 2) * 100
-            infoframe['% repeat entries'][index] = float(repeat)/(len(drop) - 1) * 100
-            infoframe['% 1 entries'][index] = float(arm_dict[1]) / len(drop) * 100
-            infoframe['% 2 entries'][index] = float(arm_dict[2]) / len(drop) * 100
-            infoframe['% 3 entries'][index] = float(arm_dict[3]) / len(drop) * 100
-            infoframe['% 4 entries'][index] = float(arm_dict[4]) / len(drop) * 100
-            infoframe['arm entry inequality'][index] = per_entry_diff / 4 * 100
+    def build_scored_df():
+        # builds the scored dataframe
+        weight_percentage = self.get_weight_perc(self.raw_df)
+        group, has_groups = self.get_group(self.raw_df)
 
-    return infoframe.astype(float)
+        # handles rescoring
+        if self.rescore == 'full':
+            scored = self.score(self.raw_df)
+        elif self.rescore == 'score_6':
+            scored = self.score(self.raw_df, rescore6 = True)
+        elif self.rescore == 'score_12':
+            scored = self.score(self.raw_df, rescore12 = True)
+
+        scored = pd.concat([group, weight_percentage, scored], axis=1)
+        scored = scored.dropna(thresh = 6, axis = 0)
+        return scored
+
+    def get_descriptive_stats():
+        grouped = self.scored_df.groupby('group').describe()
+        grouped = stdev_2_stderror(grouped)
+        return grouped
+
+    #function to score spontaneous alternation
+    def score(start = 1, rescore = 76, rescore6 = False, rescore12 = False):
+        start = str(start)
+        rescore = str(rescore)
+
+        #build dataframe to hold rescored values
+        infoframe = pd.DataFrame(index = self.raw_df.index,
+                                 columns = ['alternations',
+                                            'arm entries',
+                                            '% alternation',
+                                            '% perseverative errors',
+                                            '% repeat entries',
+                                            '% 1 entries',
+                                            '% 2 entries',
+                                            '% 3 entries',
+                                            '% 4 entries',
+                                            'arm entry inequality'])
+
+        #score SA raw data from excel file and place in infoframe
+        for index in self.raw_df.index:
+            if rescore6:
+                rescore = self.raw_df['6min_arm'].ix[index]
+                try:
+                    rescore = str(int(rescore))
+                except:
+                    rescore = None
+            if rescore12:
+                rescore = self.raw_df['12min_arm'].ix[index]
+                try:
+                    rescore = str(int(rescore))
+                except:
+                    rescore = None
+
+            drop = self.raw_df.loc[:, start:rescore].ix[index].dropna(how='all')
+
+            if len(drop) > 4:
+                alts = 0
+                possible_alts = len(drop) - 3
+                pers = 0
+                repeat = 0
+                arm_dict = {1:0, 2:0, 3:0, 4:0}
+                for n in xrange(0, len(drop)):
+                    if drop.iloc[n] not in [1, 2, 3, 4]:
+                        raise NameError('invalid arm entry input for subject %s on choice #%s: %s'
+                                         % (index, n, drop.iloc[n]))
+                    arm_dict[drop.iloc[n]] += 1
+                    if n >= 2:
+                        if drop.iloc[n] == drop.iloc[n-1]:
+                            repeat += 1
+                        if n >= 3:
+                            if drop.iloc[n] == drop.iloc[n-2]:
+                                pers +=1
+                            if n >= 4:
+                                alt_list = drop.iloc[n-3:n].values
+                                #alt_list = [drop.iloc[n], drop.iloc[n-1], drop.iloc[n-2], drop.iloc[n-3]]
+                                alt_count = collections.Counter(alt_list).values()
+                                if True not in map(lambda x: x > 1, alt_count):
+                                    alts += 1
+                per_entry_diff = sum([abs(float(x)/len(drop) - .25)
+                                      for x in arm_dict.values()])
+                infoframe['alternations'][index] = int(alts)
+                infoframe['arm entries'][index] = len(drop)
+                infoframe['% alternation'][index] = float(alts)/possible_alts * 100
+                infoframe['% perseverative errors'][index] = float(pers)/(len(drop) - 2) * 100
+                infoframe['% repeat entries'][index] = float(repeat)/(len(drop) - 1) * 100
+                infoframe['% 1 entries'][index] = float(arm_dict[1]) / len(drop) * 100
+                infoframe['% 2 entries'][index] = float(arm_dict[2]) / len(drop) * 100
+                infoframe['% 3 entries'][index] = float(arm_dict[3]) / len(drop) * 100
+                infoframe['% 4 entries'][index] = float(arm_dict[4]) / len(drop) * 100
+                infoframe['arm entry inequality'][index] = per_entry_diff / 4 * 100
+
+        return infoframe.astype(float)
+
+
+    def convert_index(df):
+        '''
+        input: dataframe
+        output: dataframe
+
+        changes index values to strings
+        '''
+        new_ix = [str(i) for i in df.index]
+        df.index = new_ix
+        return df
+
+    def get_weight_perc(df):
+        '''
+        input: df
+        output: Series
+
+        calculates weight percentages based on pre and post weights if in
+        dataframe. Otherwise returns empty strings
+        '''
+        if False in df['pre_weight'].isnull().values and \
+           False in df['post_weight'].isnull().values:
+            weight_percentage = df['post_weight'] / df['pre_weight']
+        else:
+            weight_percentage = pd.Series(['' for ix in df.index],
+                                          index = df.index)
+        weight_percentage.name = 'weight_percentage'
+        return weight_percentage
+
+    def get_group(df):
+        if False in df['group'].isnull().values:
+            group = df['group']
+            group = group.replace(np.nan, 'not in group')
+            self._has_groups = True
+        else:
+            group = pd.Series(['' for ix in df.index], index = df.index)
+            self._has_groups = False
+        group.name = 'group'
+        return group
+
+    def stdev_2_stderror(describe_df):
+        dt = describe_df.T.copy()
+        for x in dt.columns.levels[0]:
+            count = dt[x]['count'][0]
+            dt[x, 'std'] = dt[x]['std'].div(np.sqrt(count))
+
+        new_levels = [name if name != 'std' else 'sterr' for name in dt.columns.levels[1]]
+        dt.columns.set_levels(new_levels, level=1, inplace = True)
+        return dt.T
